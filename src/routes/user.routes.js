4fceac932a0de86bdcded7b9b2bda91277fbbeda
DHRUVCHARNE <dhruv4ne@gmail.com>
2024-08-11 15:49:05 +0530

    Fixed a Bug in Routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -38,7 +38,7 @@ router
    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
    .route("/cover-image")
-   .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+   .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
